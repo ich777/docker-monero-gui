0e404cfc88f00ad15d997ffb32db905ab6974c3b
ich777 <christophhummer@gmail.com>
2022-08-05 10:41:11 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
-export DISPLAY=:99
+export DISPLAY=:0
 export XAUTHORITY=${DATA_DIR}/.Xauthority
 
+LAT_V="$(wget -qO- https://api.github.com/repos/monero-project/monero-gui/releases/latest | jq -r '.tag_name')"
+CUR_V="$(find ${DATA_DIR}/bin -maxdepth 1 -type f -name "monero-guiv_*" 2>/dev/null | cut -d '_' -f2)"
+
+if [ -z "${LAT_V}" ]; then
+  if [ -z "${CUR_V}" ]; then
+    echo "---Can't get latest version from Monero-GUI and found no local installed version!---"
+	sleep infinity
+  else
+    echo "---Can't get latest version from Monero-GUI, falling back to installed version ${CUR_V}---"
+	LAT_V="${CUR_V}"
+  fi
+fi
+
+echo "---Version Check---"
+if [ -z "${CUR_V}" ]; then
+  echo "---Monero-GUI not installed, installing...---"
+  cd ${DATA_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2 "https://downloads.getmonero.org/gui/monero-gui-linux-x64-${LAT_V}.tar.bz2" ; then
+    echo "---Sucessfully downloaded Monero-GUI---"
+  else
+    echo "---Something went wrong, can't download Monero-GUI, putting container in sleep mode---"
+    sleep infinity
+  fi
+  mkdir -p ${DATA_DIR}/bin
+  tar -C ${DATA_DIR}/bin --strip-components=1 -xvf ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2
+  rm -rf ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2
+  touch ${DATA_DIR}/bin/monero-guiv_${LAT_V}
+elif [ "${CUR_V}" != "${LAT_V}" ]; then
+  echo "---Version missmatch, installed v${CUR_V}, downloading and installing latest v${LAT_V}...---"
+  cd ${DATA_DIR}
+  rm -rf ${DATA_DIR}/bin
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2 "https://downloads.getmonero.org/gui/monero-gui-linux-x64-${LAT_V}.tar.bz2" ; then
+    echo "---Sucessfully downloaded Monero-GUI---"
+  else
+    echo "---Something went wrong, can't download Monero-GUI, putting container in sleep mode---"
+    sleep infinity
+  fi
+  mkdir -p ${DATA_DIR}/bin
+  tar -C ${DATA_DIR}/bin --strip-components=1 -xvf ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2
+  rm -rf ${DATA_DIR}/monero-gui_${LAT_V}.tar.bz2
+  touch ${DATA_DIR}/bin/monero-guiv_${LAT_V}
+elif [ "${CUR_V}" == "${LAT_V}" ]; then
+	echo "---Monero-GUI v$CUR_V up-to-date---"
+fi
 
 if [ "${CUSTOM_RES_W}" -le 1279 ]; then
 	echo "---Width to low must be a minimal of 1280 pixels, correcting to 1024...---"
@@ -23,7 +67,7 @@ fi
 screen -wipe 2&>/dev/null
 
 echo "---Starting TurboVNC server---"
-vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
+vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :0 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
 sleep 2
 echo "---Starting Fluxbox---"
 env HOME=/etc /usr/bin/fluxbox 2>/dev/null &
@@ -35,5 +79,6 @@ sleep 2
 echo "---Container under construction!---"
 sleep infinity
 
-echo "---Starting MyCrypto---"
+echo "---Starting Monero-GUI---"
 cd ${DATA_DIR}
+${DATA_DIR}/bin/monero-wallet-gui
